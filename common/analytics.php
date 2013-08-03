c2ab0aaec99b6db8a01139756858ffb3b78581da
Kenta Suzuki <ken05216@gmail.com>
2013-08-03 14:46:01 +0900

    Update analytics.php

--- common/analytics.php
+++ common/analytics.php
@@ -1,33 +1,15 @@
-<div id="switch_board">
-<div id="toggle_aside">
-  <div>
-		<span class="label"></span><br>
-		side menu
-	</div>
-</div>
-</div>
 
+<!-- analytics script -->
 <script type="text/javascript">
-$(function() {
-	var target = $('#content_wrap');
-	var toggleAside = $('#toggle_aside');
-	var label = toggleAside.find('.label');
-	var label_htmls = {
-		open: '<i class="icon-chevron-right"></i> OPEN',
-		close: '<i class="icon-chevron-left"></i> CLOSE'
-	};
-	var closed = false;
-	toggleAside.click(function() {
-		if (closed) {	// open
-			target.removeClass('aside_closed');
-			label.html(label_htmls.close);
-			closed = false;
-		} else {		// close
-			target.addClass('aside_closed');
-			label.html(label_htmls.open);
-			closed = true;
-		}
-	});
-	label.html(label_htmls.close);
-});
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28916729-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>

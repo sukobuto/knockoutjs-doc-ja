f85be107460e19f57ec16be9947e2ce85da14c45
Kenta Suzuki <sukobuto@gmail.com>
2014-11-16 12:44:24 +0900

    remove unarranged files

--- common/footer.php
+++
@@ -1,5 +0,0 @@
-
-	<footer>
-		This site is unofficial Japanese translation of <a href="http://knockoutjs.com">knockoutjs.com</a>.
-		&nbsp; | &nbsp; &copy; Copyright 2012 <a href="http://sukobuto.com">sukobuto.com</a>.
-	</footer>
